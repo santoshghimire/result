861a36d1c82c5d711c02662eed154eaded96d1a7
Roshan Bhandari <brishi98@gmail.com>
2014-09-08 18:14:24 +0545

    changes for supplementary result

--- result_project/mainapp/slcntc.py
+++ result_project/mainapp/slcntc.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import requests
 
 
-def get_html_ntc(symbol_no, date_of_birth):
+def get_html_ntc(symbol_no, date_of_birth, result_type='supple'):
     try:
         # request_url = 'http://slc.ntc.net.np/slc2070_ledger.php'
         request_url  = 'http://slc.ntc.net.np/slc2070supp_ledger.php'
@@ -19,39 +19,63 @@ def get_html_ntc(symbol_no, date_of_birth):
         matches = soup.findAll('td', {'valign':'top'})
         html_parser = HTMLParser.HTMLParser()
         
+        if result_type == 'supple':
+            subjects = html_parser.unescape(str(matches[0])).replace('<td valign="top" width="50%">','').replace('</td>','').split('<br><br>')[0:8]
+        else:
+            subjects = html_parser.unescape(str(matches[0])).replace('<td valign="top">','').replace('</td>','').split('<br><br>')[0:8]
 
-        subjects = html_parser.unescape(str(matches[0])).replace('<td valign="top">','').replace('</td>','').split('<br><br>')[0:8]
-        if len(subjects) == 1:
+        if len(subjects) == 1 and result_type=='supple':
+            subjects = html_parser.unescape(str(matches[0])).replace('<td valign="top" width="50%">','').replace('</td>','').split('<br/><br/>')[0:8]
+        else:
             subjects = html_parser.unescape(str(matches[0])).replace('<td valign="top">','').replace('</td>','').split('<br/><br/>')[0:8]
         
         new_subjects = []
         for eachSubject in subjects:
             eachSubject = eachSubject.replace("\n",'').replace('\t','').replace('<br>','').replace('<br/>','').replace('</br>','')
-            new_subjects.append(eachSubject)
+            if eachSubject != '':
+                new_subjects.append(eachSubject)
 
+        if result_type == 'supple':
+            theory = html_parser.unescape(str(matches[1])).replace('<td valign="top" width="20%"><div align="center">','').replace('</div></td>','').split('<br><br>')[0:8]
+        else:
+            theory = html_parser.unescape(str(matches[1])).replace('<td valign="top"><div align="center">','').replace('</div></td>','').split('<br><br>')[0:8]
         
-        theory = html_parser.unescape(str(matches[1])).replace('<td valign="top"><div align="center">','').replace('</div></td>','').split('<br><br>')[0:8]
-        if len(theory) == 1:
+        if len(theory) == 1 and result_type == 'supple':
+            theory = html_parser.unescape(str(matches[1])).replace('<td valign="top" width="20%"><div align="center">','').replace('</div></td>','').split('<br/><br/>')[0:8]
+        else:
             theory = html_parser.unescape(str(matches[1])).replace('<td valign="top"><div align="center">','').replace('</div></td>','').split('<br/><br/>')[0:8]
         theory_marks = []
+
         for eachSubject in theory:
             eachSubject = eachSubject.replace("\n",'').replace('\t','').replace('<br>','').replace('<br/>','').replace('</br>','')
-            theory_marks.append(eachSubject)
+            if eachSubject != '':
+                theory_marks.append(eachSubject)
 
         practical = html_parser.unescape(str(matches[2])).replace('<td valign="top"><div align="center">','').replace('</div></td>','').split('<br><br>')[0:8]
-        if len(practical) == 1:
-            practical = html_parser.unescape(str(matches[2])).replace('<td valign="top"><div align="center">','').replace('</div></td>','').split('<br/><br/>')[0:8]
+
+        if len(practical) == 1 and result_type=='supple':
+            practical = html_parser.unescape(str(matches[2])).replace('<td valign="top" width="10%">','').replace('</td>','').split('<br/><br/>')[0:8]
+        else:
+            practical = html_parser.unescape(str(matches[2])).replace('<td valign="top"><div align="center">','').replace('</div></td>','').split('<br/><br/>')[0:8]                    
 
         practical_marks = []
+        
         for eachSubject in practical:
             eachSubject = eachSubject.replace("\n",'').replace('\t','').replace('<br>','').replace('<br/>','').replace('</br>','')
-            practical_marks.append(eachSubject)
+            if eachSubject != '':
+                practical_marks.append(eachSubject)
 
         
-        total = html_parser.unescape(str(matches[3])).replace('<td valign="top"> <div align="right">','').replace('</div></td>','').split('<br><br>')[0:8]
-        if len(total) ==1:
-            total = html_parser.unescape(str(matches[3])).replace('<td valign="top"> <div align="right">','').replace('</div></td>','').split('<br/><br/>')[0:8]
+        
+        try:
+            total = html_parser.unescape(str(matches[3])).replace('<td valign="top"> <div align="right">','').replace('</div></td>','').split('<br><br>')[0:8]
+            if len(total) ==1:
+                total = html_parser.unescape(str(matches[3])).replace('<td valign="top"> <div align="right">','').replace('</div></td>','').split('<br/><br/>')[0:8]
+        except:
+            total = []
+
         total_marks = []
+
         for eachSubject in total:
             eachSubject = eachSubject.replace("\n",'').replace('\t','').replace('<br>','').replace('<br/>','').replace('</br>','')
             total_marks.append(eachSubject)
@@ -63,8 +87,13 @@ def get_html_ntc(symbol_no, date_of_birth):
             marks_dic = {}
             marks_dic['subject'] = new_subjects[i]        
             marks_dic['theory'] =  theory_marks[i]
-            marks_dic['practical'] = practical_marks[i]
-            marks_dic['total_obtained'] = total_marks[i]
+            try:
+                marks_dic['practical'] = practical_marks[i]
+                marks_dic['total_obtained'] = total_marks[i]
+            except:
+                marks_dic['practical'] = ''
+                marks_dic['total_obtained'] = theory_marks[i]
+
             return_list.append(marks_dic)
         
         student_details = {"name":"", "sex":"","symbol":symbol_no,"district":"", "street":"", "school":"", "status":"ok", 'scores':return_list}
@@ -73,4 +102,4 @@ def get_html_ntc(symbol_no, date_of_birth):
     except:
         return {'status':"error", 'message':'no result'}
 
-# print get_html_ntc(symbol_no='0295639', date_of_birth='2054-09-20')
+print get_html_ntc(symbol_no='0412286D', date_of_birth='2056-02-10')
